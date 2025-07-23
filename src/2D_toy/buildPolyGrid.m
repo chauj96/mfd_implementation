4d347c8099a34f2f74d537d7d654f546f441e6be
Omar Duran <omaryesiduran@gmail.com>
2025-07-22 23:47:14 -0700

    fix orientation in polygonal grid

--- src/2D_toy/buildPolyGrid.m
+++ src/2D_toy/buildPolyGrid.m
@@ -4,101 +4,80 @@ function [cell_struct, face_struct, vertices, cells] = buildPolyGrid(domain, n_c
     maxIter = 30; 
     [vertices, cells, ~, ~, cell_centers] = PolyMesher(domain, n_cells, maxIter);
     
-    % FACE STORAGE: center, faces, volume, normal
+    % Initialize face and cell storage
     face_map = containers.Map();
     face_count = 0;
     face_struct = struct('nodes', {}, 'cells', {}, 'center', {}, 'normal', {}, 'area', {});
     cell_struct = struct('center', {}, 'faces', {}, 'faces_orientation', {}, 'volume', {});
-    
-    for c = 1:n_cells
-        verts = cells{c}; % vertex indices (global) of cell c
-        n_verts = length(verts);
-        
-        for i = 1:n_verts
-            v1 = verts(i);
-            v2 = verts(mod(i, n_verts) + 1); % wrap around
-    
-            % Enforce ordering so that v1 < v2
-            if v2 < v1
-                tmp = v1;
-                v1 = v2;
-                v2 = tmp;
-                reversed = true;
-            else
-                reversed = false;
-            end
-            
-            % Unique key for the edge
-            key1 = sprintf('%d_%d', v1, v2);
-            
-            if isKey(face_map, key1)
-                f_id = face_map(key1);
-                face_struct(f_id).cells(end+1) = c;
-            else
-                face_count = face_count + 1;
-                face_map(key1) = face_count;
-    
-                % build new face properties
-                x1 = vertices(v1,:);
-                x2 = vertices(v2,:);
-                center = 0.5 * (x1 + x2);
-                edge_vec = x2 - x1;
-                
-                % Normal vector is 90 deg CCW rotation of edge vector
-                normal = [edge_vec(2), -edge_vec(1)];
-                normal = normal / norm(normal);
-                
-                % If edge was reversed, flip normal direction to keep
-                % consistent orientation from original v1->v2 ordering
-                if reversed
-                    normal = -normal;
-                end
-                
-                area = norm(edge_vec);
-    
-                face_struct(face_count).nodes = [v1, v2];
-                face_struct(face_count).cells = [c];
-                face_struct(face_count).center = center(:);
-                face_struct(face_count).normal = normal(:);
-                face_struct(face_count).area = area;
-            end
-        end
-    end
-    
-    % CELL STORAGE: center, faces, faces_orientation, volume
+
     for c = 1:n_cells
         verts = cells{c};
         n_verts = length(verts);
-    
-        cell_struct(c).center = cell_centers(c,:)';
+        xc = cell_centers(c,:)';
+
+        for i = 1:n_verts
+            v1 = verts(i);
+            v2 = verts(mod(i, n_verts) + 1); % wrap around
+
+            edge = [v1, v2];
+            edge_sorted = sort(edge);
+            key = sprintf('%d_%d', edge_sorted(1), edge_sorted(2));
+
+            if isKey(face_map, key)
+                f = face_map(key);
+                face_struct(f).cells(end+1) = c;
+            else
+                face_count = face_count + 1;
+                f = face_count;
+                face_map(key) = f;
+
+                p1 = vertices(edge_sorted(1),:);
+                p2 = vertices(edge_sorted(2),:);
+                t = p2 - p1;
+
+                % Compute normal by 90-degree CCW rotation
+                normal = [-t(2); t(1)];
+                normal = normal / norm(normal);
+
+                center = 0.5 * (p1 + p2);
+                area = norm(t);
+
+                face_struct(f).nodes = [v1, v2];
+                face_struct(f).cells = [c];
+                face_struct(f).center = center(:);
+                face_struct(f).normal = normal(:);
+                face_struct(f).area = area;
+            end
+        end
+    end
+
+    % Assemble cell data
+    for c = 1:n_cells
+        verts = cells{c};
+        n_verts = length(verts);
+
+        xc = cell_centers(c,:)';
+        cell_struct(c).center = xc;
         cell_struct(c).volume = polyarea(vertices(verts,1), vertices(verts,2));
-    
-        cell_struct(c).faces = zeros(1, n_verts);
-    
-        % Build face indices for the cell
+
+        face_ids = zeros(1, n_verts);
+        faces_orientation = zeros(1, n_verts);
+
         for i = 1:n_verts
             v1 = verts(i);
             v2 = verts(mod(i, n_verts) + 1);
-    
-            % Enforce same ordering as before for key
-            if v2 < v1
-                tmp = v1; v1 = v2; v2 = tmp; end
-    
-            key1 = sprintf('%d_%d', v1, v2);
-            f_id = face_map(key1);
-    
-            cell_struct(c).faces(i) = f_id;
+            edge_sorted = sort([v1, v2]);
+            key = sprintf('%d_%d', edge_sorted(1), edge_sorted(2));
+            f_id = face_map(key);
+
+            nf = face_struct(f_id).normal;
+            xf = face_struct(f_id).center(:);
+            faces_orientation(i) = sign(dot(nf, xf - xc));
+
+            face_ids(i) = f_id;
         end
-    
-        % Compute faces_orientation based on vertex ordering (original)
-        edge_list = cell(1, n_verts);
-        for i = 1:n_verts
-            v1 = verts(i);
-            v2 = verts(mod(i, n_verts) + 1);
-            edge_list{i} = [v1, v2];
-        end
-        faces_orientation = cellfun(@(e) sign(e(2) - e(1)), edge_list);
-    
+
+        cell_struct(c).faces = face_ids;
         cell_struct(c).faces_orientation = faces_orientation;
     end
 end
