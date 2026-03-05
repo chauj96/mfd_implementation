f3ce7ecf7a73685e905722265c63f9c4b7a28e54
Nicola Castelletto <castelletto1@llnl.gov>
2026-03-05 10:47:48 -0800

    Remove for loop in neumannBoundary.m

--- src/3D_toy/neumannBoundary.m
+++ src/3D_toy/neumannBoundary.m
@@ -1,16 +1,17 @@
-function [A, rhs_BC] = neumannBoundary(A, ~, face_struct)
+function [A, rhs_BC, f_ids] = neumannBoundary(A, ~, face_struct)
 
     n_faces = length(face_struct);
     rhs_BC = zeros(n_faces,1);
 
     f_ids = find(arrayfun(@(x) ~isempty(x.BC_flux), face_struct));
+    f_vals = [face_struct(f_ids).BC_flux];
 
-    for idx = 1:length(f_ids)
-        f = f_ids(idx);
-        A(f, :) = 0;
-        A(:, f) = 0;
-        A(f, f) = 1;
+    % for idx = 1:length(f_ids)
+    %     f = f_ids(idx);
+        A(f_ids, :) = 0;
+        A(:, f_ids) = 0;
+        A(f_ids, f_ids) = speye(length(f_ids));
 
-        rhs_BC(f) = face_struct(f).BC_flux;
-    end
+        rhs_BC(f_ids) = f_vals;
+    % end
 end
